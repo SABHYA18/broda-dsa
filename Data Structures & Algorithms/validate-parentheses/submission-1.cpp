de874294eceddc3e412e81da677d002e22706ec5
SABHYA MAHAJAN <89938782+SABHYA18@users.noreply.github.com>
2026-08-11 22:01:45 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.size()==1 || s=="") return false;
+        stack<char> st;
+        for(int i = 0; i<s.size(); i++){
+            if(s[i]=='('||s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+                else if(st.top()=='(' && s[i]==')') st.pop();
+                else if(st.top()=='[' && s[i]==']') st.pop();
+                else if(st.top()=='{' && s[i]=='}') st.pop();
+                else return false;
+
+            }
+           
+        }
+
+        return st.empty();
+    }
+};
